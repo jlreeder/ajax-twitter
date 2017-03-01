71c6d8af599cdc96a4eaee3e37e0b5e6c25330a4
Jason Reeder <info@jasonreeder.com>
2017-03-01 15:16:27 -0800

    Add APIUtil & promises

--- frontend/follow_toggle.js
+++ frontend/follow_toggle.js
@@ -1,8 +1,10 @@
+const APIUtil = require("./api_util.js");
+
 class FollowToggle {
   constructor(el) {
     this.$el = $(el);
-    this.userId = this.$el.attr("data-user-id");
-    this.followState = this.$el.attr("data-initial-follow-state");
+    this.userId = this.$el.data("user-id");
+    this.followState = this.$el.data("initial-follow-state");
     this.render();
     this.$el.on("click", this.handleClick.bind(this));
   }
@@ -14,20 +16,18 @@ class FollowToggle {
 
   handleClick(event) {
     event.preventDefault();
-    let method = this.followState === 'unfollowed' ? 'POST' : 'DELETE';
-
-    $.ajax({
-      method: method,
-      url: `/users/${this.userId}/follow`,
-      dataType: "json",
-      data: {
-        followee_id: this.userId
-      },
-      success: () => {
+    if (this.followState === 'unfollowed') {
+      // debugger
+      APIUtil.followUser(this.userId).then( () => {
         this._toggleState();
         this.render();
-      }
-    });
+      });
+    } else {
+      APIUtil.unfollowUser(this.userId).then( () => {
+        this._toggleState();
+        this.render();
+      });
+    }
   }
 
   _toggleState() {
