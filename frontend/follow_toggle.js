4bd4f2648e48379a5b764f24156c9939950da05c
Jason Reeder <info@jasonreeder.com>
2017-03-01 14:08:39 -0800

    Enable follow/unfollow button

--- frontend/follow_toggle.js
+++ frontend/follow_toggle.js
@@ -3,7 +3,37 @@ class FollowToggle {
     this.$el = $(el);
     this.userId = this.$el.attr("data-user-id");
     this.followState = this.$el.attr("data-initial-follow-state");
+    this.render();
+    this.$el.on("click", this.handleClick.bind(this));
   }
-}
 
+  render() {
+    const text = this.followState === "unfollowed" ? "Follow" : "Unfollow";
+    this.$el.html(text);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    let method = this.followState === 'unfollowed' ? 'POST' : 'DELETE';
+
+    $.ajax({
+      method: method,
+      url: `/users/${this.userId}/follow`,
+      dataType: "json",
+      data: {
+        followee_id: this.userId
+      },
+      success: () => {
+        this._toggleState();
+        this.render();
+      }
+    });
+  }
+
+  _toggleState() {
+    this.followState = this.followState === "unfollowed" ?
+                                            "followed" : "unfollowed";
+  }
+
+} //closes class
 module.exports = FollowToggle;
